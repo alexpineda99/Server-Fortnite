78124f2d41c8ff1d7270fa91d1e142c54130e4b4
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-10-25 20:23:18 -0430

    Server config

--- src/config/connection.js
+++ src/config/connection.js
@@ -4,7 +4,7 @@ module.exports  = mysql.createPool({
   connectionLimit : 10,
   host            : 'bedtos3chjkx87gmokkt-mysql.services.clever-cloud.com',
   user            : 'uzkwksd9p1duj5fe',
-  password        : 'uzkwksd9p1duj5fe',
+  password        : 'gnKR97r0iR4ezAE5Ioxj',
   database        : 'bedtos3chjkx87gmokkt'
 });
 
