2edde330cde870431d32cd82b29a3de7aceb2ade
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-10-27 12:16:22 -0430

    Server config

--- server.js
+++ server.js
@@ -5,15 +5,7 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json({"limit":"1mb"}));
 app.disable('x-powered-by');
-const PORT = process.env.PORT || 3000;
-
-//Routes
-const register = require('./src/routes/register');
-const login = require('./src/routes/login');
-const challenges = require('./src/routes/challenges');
-const item = require('./src/routes/Item');
-const emailsender = require('./src/routes/email-server/email');
-const verification = require('./src/routes/verify');
+const PORT = process.env.PORT || 3001;
 
 app.all('*', function(_, res, next){
 	res.header('Access-Control-Allow-Origin', '*');
@@ -23,11 +15,19 @@ app.all('*', function(_, res, next){
 	next();
 });
 
+//Routes
+const register = require('./src/routes/register');
+const login = require('./src/routes/login');
+const challenges = require('./src/routes/challenges');
+const item = require('./src/routes/Item');
+const emailsender = require('./src/routes/email-server/email');
+const verification = require('./src/routes/verify');
+
 app.post("/register", register.dataValid, register.registerUser, emailsender.sendEmail);
 app.post("/login", login.dataValid, login.loginUser);
 app.post("/verify", verification.verifyinfo);
 app.get("/challenges", middle.authHeader, middle.validSign, challenges.challenges);
-// app.get("/item/:id", middle.authHeader, middle.validSign, item.Itemparams);
+app.get("/item/:id", middle.authHeader, middle.validSign, item.Itemparams);
 app.get("/item/:id", item.Itemparams);
 app.get("/verify/:id", verification.verifyinfo);
 
