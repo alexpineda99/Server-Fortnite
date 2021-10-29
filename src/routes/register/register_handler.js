896bad3929dafa20a77f9e460d39402657fc8825
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-10-28 22:23:23 -0430

    server config

--- src/routes/register/register_handler.js
+++ src/routes/register/register_handler.js
@@ -90,7 +90,19 @@ module.exports.registerUser = function (req, res) {
 
         } else {
         emailSender.sendEmail(email, hashed, name)
-
+        .then(data => {
+            if (data === null || data === undefined) {
+                res.send({
+                    success: false,
+                    msg: "Data no valid"
+                })
+            } else {
+                res.send({
+                    success: true,
+                    msg: "User succesfully registered and email sent"
+                })
+            }
+        })
         .catch(err => {
             console.log(err)
             res.send({
