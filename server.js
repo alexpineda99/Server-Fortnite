4519838517d672d30e54643bffe2a2260e5b1307
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-10-29 01:08:12 -0430

    server config

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const PORT = process.env.PORT || 3001;
 
 app.all('*', function(_, res, next){
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-	res.header("Access-Control-Allow-Headers", "Origin, content-type, auth, Content-Length, X-Requested-With");
 	next();
 });
 
