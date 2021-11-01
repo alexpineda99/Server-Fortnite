cb19ea5e0aae3e62a9b084b18dc237ea2712b6ae
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-11-01 16:59:40 -0430

    server config

--- src/routes/register/register_handler.js
+++ src/routes/register/register_handler.js
@@ -91,28 +91,21 @@ module.exports.registerUser = function (req, res) {
         } 
         
         else {
-            console.log("registro aca")
-        // emailSender.sendEmail(email, hashed, name)
-        // .then(data => {
-        //     if (data === null || data === undefined) {
-        //         res.send({
-        //             success: false,
-        //             msg: "Data no valid"
-        //         })
-        //     } else {
-        //         res.send({
-        //             success: true,
-        //             msg: "User succesfully registered and email sent"
-        //         })
-        //     }
-        // })
-        // .catch(err => {
-        //     console.log(err)
-        //     res.send({
-        //         success: err,
-        //         msg: "Error in server"
-        //     })
-        // })
+            console.log("registrando")
+        emailSender.sendEmail(email, hashed, name)
+        .then(data => {
+                res.send({
+                    success: true,
+                    msg: "User succesfully registered and email sent"
+                })
+        })
+        .catch(err => {
+            console.log(err)
+            res.send({
+                success: err,
+                msg: "Error in server"
+            })
+        })
 
 
     }
