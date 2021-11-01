d2fd3f983543cafa73641261dacb4c7d52896ac4
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-11-01 17:45:14 -0430

    server config

--- src/routes/email-server/email.js
+++ src/routes/email-server/email.js
@@ -2,50 +2,27 @@ const nodemailer = require("nodemailer");
 const transport = require("./config");
 const emailTemplate = require("../../Templates/verifyEmailTemplate");
 
-// exports.sendEmail = function (email, data, name) {
-  exports.sendEmail = function () {
-  
+exports.sendEmail = function (email, data, name) {
+  return new Promise ((resolve, reject) => {
 
+  try {
   let transporter = nodemailer.createTransport(transport.infouser());
 
-  // let mailOptions = {
-  //   from: 'alex.fortine.app@gmail.com', // sender address
-  //   to: email, // list of receivers
-  //   subject: "Verify your Fortnite´s email", // Subject line
-  //   html: emailTemplate.verifytemplate(data, name), // html body
-  // }
+    transporter.verify(async function(error, success) {
+        // send mail with defined transport object
+      let info = await transporter.sendMail({
+          from: 'alex.fortine.app@gmail.com', // sender address
+          to: email, // list of receivers
+          subject: "Verify your Fortnite´s email", // Subject line
+          html: emailTemplate.verifytemplate(data, name), // html body
+        });
+    console.log("Server is ready to take our messages");
+    return resolve(success)
+    });
 
-  let mailOptions = {
-    from: 'alex.fortine.app@gmail.com', // sender address
-    to: "alexandropinedam1310@gmail.com", // list of receivers
-    subject: "Verify your Fortnite´s email", // Subject line
-    html: emailTemplate.verifytemplate("correo", "Alex"), // html body
+  } catch(error) {
+    console.log(error);
+    return reject(error)
   }
-
-  transporter.verify(mailOptions, function(err, data) {
-
-    if (err) {
-      console.log("Error sending email: ", err)
-    } else {
-      console.log("Email verification sent successfully")
-    }
-
-
-  });
-
-    // transporter.verify(async function(error, success) {
-    //     // send mail with defined transport object
-    //   let info = await transporter.sendMail({
-    //       from: 'alex.fortine.app@gmail.com', // sender address
-    //       to: email, // list of receivers
-    //       subject: "Verify your Fortnite´s email", // Subject line
-    //       html: emailTemplate.verifytemplate(data, name), // html body
-    //     });
-
-    // resolve(success)
-    // console.log("Server is ready to take our messages");
-    // });
-
-  console.log("datos auth: " + transport.infouser())
-
+  })
 }
