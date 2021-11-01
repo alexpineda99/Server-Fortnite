174428ab898a7e65b30c06cc1811ea7c5dbee1ae
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-11-01 18:01:32 -0430

    server config

--- src/routes/email-server/config.js
+++ src/routes/email-server/config.js
@@ -1,8 +1,9 @@
+const nodemailer = require("nodemailer");
+
 // async..await is not allowed in global scope, must use a wrapper
 exports.infouser = function () {
   // create reusable transporter object using the default SMTP transport
   let transporter = {
-    host: "smtp.gmail.com",
     service: "gmail",
     auth: {
       user: "alex.fortine.app@gmail.com", // generated ethereal user
