a1bf98b337da9039d829e20e1e78bc0601c70524
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-11-01 18:19:31 -0430

    server config

--- src/routes/email-server/config.js
+++ src/routes/email-server/config.js
@@ -4,6 +4,8 @@ const nodemailer = require("nodemailer");
 exports.infouser = function () {
   // create reusable transporter object using the default SMTP transport
   let transporter = {
+    host: "smtp.gmail.com",
+    port: 465,
     service: "gmail",
     auth: {
       user: "alex.fortine.app@gmail.com", // generated ethereal user
