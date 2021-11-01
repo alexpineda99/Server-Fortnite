becae74a59a597eaac0e251ded92347a0dca4741
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-11-01 17:06:47 -0430

    server config

--- src/routes/email-server/email.js
+++ src/routes/email-server/email.js
@@ -6,20 +6,22 @@ exports.sendEmail = function (email, data, name) {
   return new Promise ((resolve, reject) => {
 
   try {
-  let transporter = nodemailer.createTransport(transport.infouser());
+  // let transporter = nodemailer.createTransport(transport.infouser());
 
-    transporter.verify(async function(error, success) {
-        // send mail with defined transport object
-      let info = await transporter.sendMail({
-          from: 'alex.fortine.app@gmail.com', // sender address
-          to: email, // list of receivers
-          subject: "Verify your Fortnite´s email", // Subject line
-          html: emailTemplate.verifytemplate(data, name), // html body
-        });
+  //   transporter.verify(async function(error, success) {
+  //       // send mail with defined transport object
+  //     let info = await transporter.sendMail({
+  //         from: 'alex.fortine.app@gmail.com', // sender address
+  //         to: email, // list of receivers
+  //         subject: "Verify your Fortnite´s email", // Subject line
+  //         html: emailTemplate.verifytemplate(data, name), // html body
+  //       });
 
-    resolve(success)
-    console.log("Server is ready to take our messages");
-    });
+  //   resolve(success)
+  //   console.log("Server is ready to take our messages");
+  //   });
+
+  console.log("datos correo: " + email + data + name)
 
   } catch(error) {
     console.log(error);
