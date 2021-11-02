77acc958e3079990cd42e53320512991deee87dc
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-11-01 21:18:39 -0430

    server test

--- server.js
+++ server.js
@@ -43,8 +43,6 @@ app.get("/test", (req, res) => {
   console.log("test")
   let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
     auth: {
       user: "alex.fortine.app@gmail.com", // generated ethereal user
       pass: "alexfortnite99",
