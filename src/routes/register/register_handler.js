6ee8960012b7f9a159cc7e7f587870f175105b9a
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-11-23 12:58:44 -0430

    Mejoras en validacion

--- src/routes/register/register_handler.js
+++ src/routes/register/register_handler.js
@@ -6,7 +6,7 @@ module.exports.dataValid = function (req, res, next) {
     const {name, country, region, phone, email, password} = req.body;
 
     let regLetter = /^[a-zA-Z\s]*$/; // regular expression only letters
-    let regPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,16}$/; // regular expression password
+    let regPass = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/; // regular expression password
     let regEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/; // regular expression email
     let regPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
 
