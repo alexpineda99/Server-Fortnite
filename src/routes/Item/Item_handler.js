e9f92b943717df65fd6c7db48bde4d8d14ded920
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-11-24 19:47:38 -0430

    Cambio en el middleware

--- src/routes/Item/Item_handler.js
+++ src/routes/Item/Item_handler.js
@@ -1,12 +1,12 @@
 exports.Itemparams = function (req, res, next) {
 
     
-    const id = req.params.id;
-    res.send({
-        msg: `this is the url ${id}`
-    })
-    console.log("great! take the url item " + id);
+    // const id = req.params.id;
+    // res.send({
+    //     msg: `this is the url ${id}`
+    // })
+    // console.log("great! take the url item " + id);
 
-    next();
+    // next();
 
 }
